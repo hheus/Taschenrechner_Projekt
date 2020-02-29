b94045b11800d467cf7171dfaaa170ea320bdd28
Holger Heus <holger.heus@unitybox.de>
2020-02-29 15:10:02 +0100

    Änderung der Addition von double nach float

--- Program.cs
+++ Program.cs
@@ -11,17 +11,17 @@ namespace Taschenrechner_Projekt
         static void Main(string[] args)
         {
             //User Story "Addieren": Als Benutzer möchte ich zwei Zahlen eingeben, um deren Summe berechnen zu lassen.
-            Console.WriteLine("Gib den ersten Summanden ein");
+            Console.Write("Gib den ersten Summanden ein");
             string ersterSummand = Console.ReadLine();
-            Console.WriteLine("Gib den zweiten Summanden ein");
+            Console.Write("Gib den zweiten Summanden ein");
             string zweiterSummand = Console.ReadLine();
 
             // Wandel Text in Gleitkommazahlen
-            double ersterSummandAlsZahl = Convert.ToDouble(ersterSummand);
-            double zweiterSummandAlsZahl = Convert.ToDouble(zweiterSummand);
+            float ersterSummandAlsZahl = Convert.ToSingle(ersterSummand);
+            float zweiterSummandAlsZahl = Convert.ToSingle(zweiterSummand);
 
             // Berechnung ausführen
-            double summe = ersterSummandAlsZahl + zweiterSummandAlsZahl;
+            float summe = ersterSummandAlsZahl + zweiterSummandAlsZahl;
 
             //Ausgabe
             Console.WriteLine("Die Summe ist: {0}", summe);
